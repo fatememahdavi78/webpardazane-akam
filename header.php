cd40bba0bccd6d9e7c4173bcdb9981626ba60d62
fatememahdavi78 <mmahdavii0101@gmail.com>
2020-10-17 23:28:03 +0330

    webpardazane akam

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/animat.css">
+</head>
+<body>
+    <header>
+    
+    <nav>
+        <div class="logoo">
+            <img src="image/llogo.png" class="logooo-img">
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="#">صفحه اصلی</a></li>
+                <li><a href="#">طراحی سایت</a></li>
+                <li><a href="#">درباره وب پردازان</a></li>
+                <li><a href="#">تماس با ما</a></li>
+            </ul>
+        </div>
+       <!-- <div class="sign">
+            <a href="#">ورود / عضویت</a>
+        </div>-->
+    </nav>
---
+++ index.php
@@ -0,0 +1,159 @@
+
+<?php
+include('header.php');
+?>
+        <div id="banner">
+            <div class="banner-right">
+                <div class="banner-txt">
+                    <span>WebPardazaneAkam</span><br>وب پردازان آکام
+                </div>
+            </div>
+            <div class="banner-left">
+                <img src="image/labtab.png" id="banner-labtap" >
+            </div>
+        </div>
+    </header>
+
+    <main>
+    <!--start main1-->
+    <div class="main1">
+        <div class="main1-right">
+            <div class="bg-circle">
+                <img src="image/113.png" class="about-img">
+            </div>
+        </div>
+        <div class="main1-left">
+            <div class="main1-left-text">
+                <p>تعرفه طراحی سایت</p>
+                <span>
+                تعرفه و هزینه طراحی وب سایت در بهترین شرکت طراحی سایت حرفه ای شامل مواردی چون:  <br> درج مطالب برای طراحی وب سایت، هزینه ثبت دامنه، پیاده سازی سایت، اختصاص هاستینگ، آموزش پنل کاربری وب سایت، اختصاص طراح گرافیک دو بعدی وب سایت در صورت نیاز، راه اندازی نسخه موبایلی وب سایت در صورت نیاز، پشتیبانی و نگهداری وب سایت می باشد.<br><br> در واقع قیمت طراحی سایت براساس قابلیت هایی که در طراحی وب سایت لحاظ می شود تعریف می گردد.<br> همچنین مدت زمان طراحی بنابر میزان قابلیت ها و فعالیت هایی که انجام می شود تعیین می گردد. <br> وب پردازان آکام از بهترین شرکت های طراحی وب سایت در تهران با بهترین نمونه کارهاست.<br><br><br>
+                </span>
+                <div class="order">
+                <a href="#">سفارش طراحی سایت</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--end main1-->
+    <!--start main2-->
+    <div class="main2">
+        <div class="main2-center">
+            <div class="box">
+                <img src="image/Square-logo.png" class="Square">
+                <div class="question">
+                    <span class="main2-text">
+                    یک سایت حرفه ای چه ویژگی ها و استانداردهایی باید داشته باشد؟
+                    </span>
+                </div>
+                <div class="line"></div>
+                <div class="response">
+                    سایت حرفه ای در نگاه اول باید مخاطب را به خود جذب کند مواردی که منجر به این امر می شوند: <br>
+                    گرافیک زیبا و صریح همراه با سرعت لود بالا به همراه دربرگرفتن مفهوم و ماهیت اصلی و ارائه خدمات سایت است.
+                </div>
+            </div>
+            <div class="box">
+                <img src="image/Square-logo.png" class="Square">
+                <div class="question"> 
+                    <span class="main2-text">
+                        دلیل تفاوت قیمت طراحی سایت در شرکت های مختلف چیست؟
+                    </span>
+                </div>
+                <div class="line"></div>
+                <div class="response">
+                     اصلی ترین دلیل تفاوت قیمت در شرکت طراحی سایت، نحوه انجام کار و پیاده سازی وب سایت است.<br>
+                      هراندازه که میزان استفاده از ابزارهای رایگان و آماده در مراحل مختلف طراحی سایت بیشتر باشد هزینه طراحی سایت و زمان تحویل سایت کاهش<br> پیدا میکند.
+                </div>
+            </div>
+            <div class="box">
+                <img src="image/Square-logo.png" class="Square">
+                <div class="question">
+                    <span class="main2-text">
+                        نحوه پرداخت هزینه طراحی سایت اختصاصی چگونه می باشد؟
+                    </span>
+                </div>
+                <div class="line"></div>
+                <div class="response">
+                هزینه طراحی سایت در سه مرحله دریافت می شود: <br>
+                ۱_  ۵۰ درصد مبلغ قرارداد به صورت پیش پرداخت <br>
+                ۲_  ۲۰ درصد بعد از ارائه طرح اولیه<br>
+                ۳_  ۳۰درصد باقی مانده در پایان کار
+                </div>
+            </div>
+        </div>
+    </div> 
+    <!--end main2-->
+
+    <!--start main3-->
+    <div class="main3">
+        <p>اصولی ترین فاکتورهای مورد استفاده در طراحی وب سایت</p>
+        <div class="list">
+            <ul>
+                <li>استانداردهای طراحی سایت<br><span>Website design standards</span></li>
+                <li>ریسپانسیو<br><span>Responsive</span></li>
+                <li>طراحی سایت اصولی<br><span>Principled site design</span></li>
+                <li>سرعت سایت<br><span>Site speed</span></li>
+            </ul>
+        </div>
+        <div class="main3-img">
+                <div class="main3-logoos wow pulse" data-wow-iteration="infinite" data-wow-delay="1s"  >
+                <img src="image/asp.png" class="logo-img">
+                <div class="wow pulse" data-wow-iteration="infinite" data-wow-delay="1s" data-wow-duration="5s">
+                <img src="image/asp.png" class="logo-img m3-logo">
+                </div>
+                <img src="image/css3.png" class="logo-img">
+                </div>
+                <div class="main3-logo2">
+                    <div class="logo-asp-php wow pulse" data-wow-iteration="infinite" data-wow-delay="1s" data-wow-duration="2s">
+                    <img src="image/php.png" class="logo-img">
+                    </div>
+                    <div class="img-res">
+                        <img src="image/respansive.png" id="img-respansive">
+                    </div>
+                    <div class="logo-asp-php wow pulse" data-wow-iteration="infinite" data-wow-delay="1s" data-wow-duration="2s" >
+                        <img src="image/asp.png" class="logo-img">
+                    </div>
+                </div>
+        </div>
+    </div>
+    <!--end main3-->
+    
+    <!--start main4-->
+    <div class="main4">
+    <p>نمونه کارهای ما</p>
+    <div class="main4-row1">
+        <div class="nemunkar">
+            <img src="image/fakher.PNG" class="nemunkar-img">
+        </div>
+        <div class="main4-txt">
+            <h3>هلدینگ فاخر</h3>
+            <span> - دو زبانه بودن وب سایت</span>
+            <span> - دو زبانه بودن وب سایت</span>
+            <span> - دو زبانه بودن وب سایت</span>
+            <div class="main4-logo">
+              <img src="image/fakher-logo-1.png" >
+              <a href="https://fakher.ir/">https://fakher.ir</a>
+             </div>
+        </div>
+    </div>
+    <div class="main4-row2"></div>
+    <div class="main4-row3"></div>
+
+    </div>
+    <!--end main4-->
+
+    </main>
+
+<?php
+include('footer.php');
+?>
+
+
+
+
+<script src="js/wow.min.js"></script>
+    <script>
+        new WOW().init();
+    </script>
+
+</body>
+</html>
